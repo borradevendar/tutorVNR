72c52ae5cb3632395ec752ac513064fe7183e49f
borradevendar <borradevendarsaiteja123@gmail.com>
2024-06-10 15:37:38 +0530

    added UserLogin and styled it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import LoginPage from './components/LoginPage';
+// import LoginPage from './components/LoginPage';
 import UserLogin from './components/UserLogin';
 export default function App() {
   return (
     <div>
-      <LoginPage/>
+      <UserLogin/>
     </div>
   );
 }
--- frontend/src/components/UserLogin.js
+++ frontend/src/components/UserLogin.js
@@ -1,12 +1,32 @@
-export default function UserLogin(){
-    return(
-        <div>
-            <form>
-                <label for="rollno">Roll Number</label>
-                <input name="rollno" type="text" id="rollno"/>
-                <label for="Password">Password</label>
-                <input name="Password" type="text" id="Password"/>
-            </form>
+export default function UserLogin() {
+  return (
+    <div className="Login">
+    
+      <form className="loginContainer">
+      <h1 className="formTitle">User Login</h1>
+        <div className="form-group">
+          <label for="rollno">Enter Your Roll Number</label>
+          <input
+            type="text"
+            className="form-control "
+            id="rollno"
+            placeholder="Roll Number"
+          />
         </div>
-    )
+        <div className="dummyContainer"></div>
+        <div className="form-group">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary LoginButton">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
